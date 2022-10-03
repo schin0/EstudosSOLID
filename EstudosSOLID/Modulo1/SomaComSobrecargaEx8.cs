b2c3af83505e68fab861e98328935c919a38a700
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-10-03 19:53:42 -0300

    #8 - Criar métodos com sobrecarga para somar

---
+++ EstudosSOLID/Modulo1/SomaComSobrecargaEx8.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EstudosSOLID.Modulo1
+{
+    public class SomaComSobrecargaEx8
+    {
+        public void Somar() => Console.WriteLine("Nenhum valor informado");
+
+        public int Somar(int num1, int num2) => num1 + num2;
+        
+        public double Somar(double num1, double num2) => num1 + num2;
+
+        public double Somar(double num1, int num2) =>  num1 + num2;
+
+        public double Somar(int num1, double num2) => num1 + num2;
+    }
+}
