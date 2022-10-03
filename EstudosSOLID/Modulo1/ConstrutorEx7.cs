0ed9269eb41f1a4c696cba61a568ab38eb9340fd
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-10-03 19:51:25 -0300

    #7 - Porque usar construtor

---
+++ EstudosSOLID/Modulo1/ConstrutorEx7.cs
@@ -0,0 +1,13 @@
+﻿namespace EstudosSOLID.Modulo1
+{
+    class ConstrutorEx7
+    {
+        // Por que você usa um construtor?
+
+        /*
+        
+        Usamos para definir propriedades iniciais ao instanciar uma classe.
+
+         */
+    }
+}
