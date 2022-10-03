83fe59a54e4fbd84e95986e6911a8e226b9c53ae
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-10-03 19:41:32 -0300

    #2 - Conceituar Abstração

---
+++ EstudosSOLID/Modulo1/AbstracaoEx2.cs
@@ -0,0 +1,12 @@
+﻿namespace EstudosSOLID.Modulo1
+{
+    class AbstracaoEx2
+    {
+        /*
+        
+        Abstração é a capacidade de extrair itens e suas características presentes
+        no contexto real, com o objetivo de criar objetos para o projeto.
+
+         */
+    }
+}
