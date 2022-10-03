f447ffee37440112807dc08f4f5e97f9ed0214d2
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-10-03 19:46:33 -0300

    #4 - Conceituar encapsulamento

---
+++ EstudosSOLID/Modulo1/EncapsulamentoEx4.cs
@@ -0,0 +1,13 @@
+﻿namespace EstudosSOLID.Modulo1
+{
+    class EncapsulamentoEx4
+    {
+        /*
+        
+        O encapsulamento serve para controlarmos a visibilidade dos 
+        membros das nossas classes. Assim, podemos garantir uma melhor
+        segurança de dados aos usuários.
+
+         */
+    }
+}
