4d410a27fc0f61ae66c0c3fe91b499ebf620240f
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-10-03 19:35:44 -0300

    Add project files.

---
+++ EstudosSOLID/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EstudosSOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
