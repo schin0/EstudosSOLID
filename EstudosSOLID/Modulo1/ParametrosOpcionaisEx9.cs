d12a6a293b8658bef1795ac2c6c49e810729b555
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-10-03 19:55:15 -0300

    #9 - Criar método para somar até 4 números

---
+++ EstudosSOLID/Modulo1/ParametrosOpcionaisEx9.cs
@@ -0,0 +1,10 @@
+﻿namespace EstudosSOLID.Modulo1
+{
+    class ParametrosOpcionaisEx9
+    {
+        
+        public static int SomarAteQuatroNumeros(int num1, int num2 = 0, int num3 = 0, int num4 = 0)
+            => num1 + num2 + num3 + num4;
+
+    }
+}
