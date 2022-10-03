bfbc509a624a7ec921cd1ff43b5c9f77bfa2c550
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-10-03 19:47:55 -0300

    #5 - Conceituar construtor

---
+++ EstudosSOLID/Modulo1/ConstrutorEx5.cs
@@ -0,0 +1,12 @@
+﻿namespace EstudosSOLID.Modulo1
+{
+    class ConstrutorEx5
+    {
+        /*
+         
+        O Construtor de uma classe é chamado ao instanciá-la num objeto. 
+        Podemos utilizar para definir os atributos iniciais de um objeto.
+
+         */
+    }
+}
