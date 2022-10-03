7c61dbaa32731493f624d8ef9b930ff2a484c7d9
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-10-03 19:49:49 -0300

    #6 - Definir chamada de construtor

---
+++ EstudosSOLID/Modulo1/ConstrutorEx6.cs
@@ -0,0 +1,13 @@
+﻿namespace EstudosSOLID.Modulo1
+{
+    class ConstrutorEx6
+    {
+        // Se você criar 5 objetos de uma classe, quantas vezes o construtor da classe será chamado ?
+
+        /*
+        
+        O construtor será chamado 5 vezes.
+
+         */
+    }
+}
