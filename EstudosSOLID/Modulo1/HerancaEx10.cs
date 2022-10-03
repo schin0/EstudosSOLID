797352c760ba5bb959c73cbf2d39fbbbe85e9872
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-10-03 19:58:39 -0300

    #10 - Conceituar herança

---
+++ EstudosSOLID/Modulo1/HerancaEx10.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EstudosSOLID.Modulo1
+{
+    class HerancaEx10
+    {
+        /*
+
+        Com a herança, conseguimos copiar/herdar todas as propriedades, métodos e
+        atributos de uma classe para outra (classe filha).
+
+        Classe Geral: Documento
+        Classe específica 1: Cliente
+        Classe específica 2: Funcionário
+
+        */
+    }
+
+    // Exemplos:
+    public class Documento
+    {
+        public string TipoDeDocumento { get; set; }
+        public string NumeroDocumento { get; set; }
+        public DateTime DataExpedicao { get; set; }
+    }
+
+    public class Cliente : Documento
+    {
+        public string NomeCliente { get; set; }
+    }
+
+    public class Funcionario : Documento
+    {
+        public string NomeFuncionario { get; set; }
+        public string CarteiraDeTrabalho { get; set; }
+    }
+}
