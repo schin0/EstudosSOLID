d713ee2120a7fcdf179585b876f885cf9b472855
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-10-03 21:46:21 -0300

    Criar exemplo de classe para UML

---
+++ EstudosSOLID/Modulo2/PessoaUML.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EstudosSOLID.Modulo2
+{
+    public class PessoaUML
+    {
+        public PessoaUML() { }
+
+        public PessoaUML(string nome, int idade, string sexo)
+        {
+            Nome = nome;
+            Idade = idade;
+            Sexo = sexo;
+        }
+
+        private DateTime dataNascimento = new DateTime(2000, 10, 2);
+
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Sexo { get; set; }
+
+        public void IdentificarPessoa()
+            => Console.WriteLine($"{Nome} - {Idade} - {Sexo}");
+
+    }
+}
