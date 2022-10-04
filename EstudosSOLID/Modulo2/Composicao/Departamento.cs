ef018ec2b47cde7a6f19520753458a3e06b82352
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-10-03 21:45:26 -0300

    Criar exemplo de Composição

---
+++ EstudosSOLID/Modulo2/Composicao/Departamento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EstudosSOLID.Modulo2.Composicao
+{
+    class Departamento : IDisposable
+    {
+        private Escola escola;
+        private string nome;
+
+        /*
+        Usamos um construtor interno para limitar como ele pode ser criado
+        Já que um departamento não terá sentido por si só     
+         */
+        internal Departamento(Escola escola, string nome)
+        {
+            this.escola = escola;
+            this.nome = nome;
+        }
+
+        // Serve para destruir a instância
+        public void Dispose() { }
+    }
+}
---
+++ EstudosSOLID/Modulo2/Composicao/Escola.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EstudosSOLID.Modulo2.Composicao
+{
+    class Escola : IDisposable
+    {
+        public string Nome { get; set; }
+        private IList<Departamento> departamentos = new List<Departamento>();
+
+        // Somente Escola poderá criar Departamentos
+        public void AdicionarDepartamento(string nome)
+            => departamentos.Add(new Departamento(this, nome));
+
+        public void Dispose()
+        {
+            foreach (var departamento in departamentos)
+                departamento.Dispose();
+        }
+    }
+}
