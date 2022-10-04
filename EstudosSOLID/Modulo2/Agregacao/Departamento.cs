51093b901cfb8bf5c7c5c299d825f0fff56ea538
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-10-03 21:45:05 -0300

    Criar exemplo de Agregação

---
+++ EstudosSOLID/Modulo2/Agregacao/Departamento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EstudosSOLID.Modulo2.Agregacao
+{
+    class Departamento : IDisposable
+    {
+        public string Nome { get; set; }
+        public List<Professor> Professores { get; set; }
+
+        public void Dispose() { }
+    }
+}
---
+++ EstudosSOLID/Modulo2/Agregacao/Professor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EstudosSOLID.Modulo2.Agregacao
+{
+    class Professor : IDisposable
+    {
+        public string Nome { get; set; }
+
+        public void Dispose() { }
+    }
+}
